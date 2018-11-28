69f66aff5684ca8a98a7d9f0dd40ab1a636e3ac1
韩常铭 <han.changming@qq.com>
2018-11-28 18:42:06 +0800

    - 创建spring-cloud-user-service工程

---
+++ spring-cloud-user-service/src/main/java/com/rengu/demo/springcloud/springclouduserservice/SpringCloudUserServiceApplication.java
@@ -0,0 +1,12 @@
+package com.rengu.demo.springcloud.springclouduserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudUserServiceApplication.class, args);
+    }
+}
