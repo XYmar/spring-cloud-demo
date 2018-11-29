a247a3a8894577496b9c58f44a989115482f31ea
韩常铭 <han.changming@qq.com>
2018-11-29 09:49:47 +0800

    - 初始化spring-cloud-user-consumer工程

---
+++ spring-cloud-user-consumer/src/main/java/com/rengu/demo/springcloud/springclouduserconsumer/SpringCloudUserConsumerApplication.java
@@ -0,0 +1,12 @@
+package com.rengu.demo.springcloud.springclouduserconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudUserConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudUserConsumerApplication.class, args);
+    }
+}
