c05973825545be2edb5b735b235d0d5af186ced3
韩常铭 <han.changming@qq.com>
2018-11-29 11:32:43 +0800

    - 初始化spring-cloud-authorization-server授权服务器工程

---
+++ spring-cloud-authorization-server/src/main/java/com/rengu/demo/springcloud/springcloudauthorizationserver/SpringCloudAuthorizationServerApplication.java
@@ -0,0 +1,12 @@
+package com.rengu.demo.springcloud.springcloudauthorizationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudAuthorizationServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudAuthorizationServerApplication.class, args);
+    }
+}
