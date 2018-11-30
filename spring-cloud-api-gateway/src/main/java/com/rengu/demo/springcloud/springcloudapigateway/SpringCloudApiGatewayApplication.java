b734656d25b4c5f73d9098314e54990c28348e5b
韩常铭 <han.changming@qq.com>
2018-11-30 14:00:44 +0800

    - 初始化spring-cloud-api-gateway工程

---
+++ spring-cloud-api-gateway/src/main/java/com/rengu/demo/springcloud/springcloudapigateway/SpringCloudApiGatewayApplication.java
@@ -0,0 +1,12 @@
+package com.rengu.demo.springcloud.springcloudapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudApiGatewayApplication.class, args);
+    }
+}
